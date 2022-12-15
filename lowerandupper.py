b1cf9abacfcf1ffdde34c0ad3fb533af29792efb
ajay22p <57622663+ajay22p@users.noreply.github.com>
2022-12-15 14:42:51 +0530

    Update lowerandupper.py

--- lowerandupper.py
+++ lowerandupper.py
@@ -3,7 +3,7 @@ from pyspark.sql import functions as f
 
 spark=SparkSession.builder.appName('loweandupper').getOrCreate()
 
-
+#reading csv
 df=spark.read.csv("/home/raja/Desktop/Ekstep/cassandra/user/diksha_user.csv",header=True,sep='|')
 
 df.filter(df.usertype == 'student').agg(f.count('userid')).show()
